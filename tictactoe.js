cbe5ac5ad93a868a89e4e6f61199c5439ea28793
Luke Seemann <bikesarefun@gmail.com>
2016-06-27 21:54:43 -0500

    Add Jasmine testing, other refactors and reorganization

--- tictactoe.js
+++ tictactoe.js
@@ -1,21 +1,25 @@
 function Board () {
   var size = 3,
-      spots = Array(null).fill(size * size),
-                 // Creates an array of null values, one for each spot on the board.
-                 // Each spot starts as null but will change to 0 or 1,
+      spots = Array(0).fill(size * size),
+                 // Creates an array of 0 values, one for each spot on the board.
+                 // Each spot starts as 0 but will change to 1 or 2,
                  // depending on who plays there.
-      characters = ['X', 'O'], // Can be changed by user.
-      humanity = [true, false], // Is the respective player a human? Can be changed by user.
-      turn = 0, // Either 0 or 1, for Player 1 and 2, respectively.
-      goesfirst = 0, // Either 0 or 1.
-      winner = null; // Either null (for an unfinished game) or 0, 1, or "Draw."
+      characters = [null, 'X', 'O'], // Can be set by user.
+      humanity = [null, true, false], // Is the respective player a human? Can be set by user.
+      turn = 1, // Either 1 or 2.
+      goesfirst = 1, // Either 1 or 2.
+      winner = 0, // Either 0 (for an unfinished game or draw), 1 or 2.
+      winningSet = [], // Will be populated once there’s a winner
+      winningSets = [ [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                      [0, 4, 8], [2, 4, 6] ];
 
   this.setTurn = function(player) {
     turn = player;
   }
 
   this.toggleTurn = function() {
-    turn = turn === 0 ? 1 : 0;
+    turn = turn === 1 ? 2 : 1;
   }
 
   this.setOwner = function(spot, owner) {
@@ -31,6 +35,30 @@ function Board () {
     return !humanity[turn];
   }
 
+  this.gameIsOver = function() {
+    return this.getOpenSpots().length === 0 || this.hasWinner();
+  }
+
+  // Returns false if game is incomplete or a draw
+  this.hasWinner = function() {
+    return winner === 1 || winner === 2;
+  }
+
+  this.getWinner = function() {
+    return winner;
+  }
+
+  // Return an array of the spots that are still unoccupied.
+  this.getOpenSpots = function() {
+    var openSpots = [];
+    for (var i = spots.length - 1; i >= 0; i--) {
+      if (spots[i] === 0) {
+        openSpots.push(i);
+      }
+    }
+    return openSpots;
+  }
+
   // Take the settings from the user and, if valid, apply them to the board.
   // Then, hide the settings, reveal the board, and commence play.
   this.loadBoard = function() {
@@ -41,12 +69,16 @@ function Board () {
         error = validateCharacters(p1_character, p2_character);
 
     if (error) {
-      announce('<span class="error">' + error + '</span>');
+      announcement.display('<span class="error">' + error + '</span>');
     }
     else {
-      characters[0] = p1_character;
-      characters[1] = p2_character;
+      characters = [
+        null,
+        p1_character,
+        p2_character
+      ],
       humanity = [
+        null,
         p1_identity === "human",
         p2_identity === "human"
       ];
@@ -55,175 +87,104 @@ function Board () {
       document.getElementById('ttt-arena').style.display = 'block';
 
       if (document.querySelector('input[name="goes-first"]:checked').value === 'p2') {
-        goesfirst = 1;
+        goesfirst = 2;
       }
 
-      this.resetBoard();
+      this.startGame();
     }
   }
 
-  function validateCharacters(characterA, characterB) {
-    if (characterA.length === 0) {
-      return 'Player 1 needs a character.';
-    }
-    if (characterB.length === 0) {
-      return 'Player 2 needs a character.';
-    }
-    if (characterA === characterB) {
-      return 'It looks like both players have the same character. Please change one.'
-    }
-    return false;
+  // Reset the configuration of the board and commence play.
+  this.startGame = function() {
+    spots = [
+      0, 0, 0,
+      0, 0, 0,
+      0, 0, 0
+    ],
+    winner = 0,
+    winningSet = [],
+    turn = goesfirst;
+
+    drawBoard(spots, characters, this.getOpenSpots(), size);
+    this.goToNextTurn();
   }
 
-  // Relay the state of the game to the UI.
-  this.drawBoard = function() {
-    clearAnnounce()
-    for (var i = spots.length - 1; i >= 0; i--) {
-      document.getElementById('pos-' + i).innerHTML = characters[spots[i]] || '&nbsp;';
-      if (spots[i] !== null) {
-        document.getElementById('pos-' + i).className += " taken";
-      }
+  this.goToNextTurn = function() {
+    // If it's now a computer's turn, find the best move and make it.
+    if (currentPlayerIsComputer()) {
+      var that = this;
+      // Give it a delay so we can observe things happening.
+      setTimeout(function () {
+        that.makeMove(that.getBestMove());
+      }, 200)
     }
-    // If this is an empty board, reset a few things.
-    if (this.getOpenSpots().length === 9) {
-      document.getElementById('reset-button').classList.remove('active');
-      document.getElementById('ttt-table').classList.remove('game-over');
-      removeClass('taken');
-      removeClass('winner');
-      removeClass('winner-0');
-      removeClass('winner-1');
-    }
+    // If it’s a human’s turn, prompt her.
     else {
-      document.getElementById('reset-button').classList.add('active');
+      announcement.display(getPrompt(turn))
     }
-  }
-
-  function removeClass(classname) {
-    var els = document.getElementsByClassName(classname);
-    while (els.length > 0) {
-      els[0].classList.remove(classname);
-    }
-  }
-
-  // Look for a winner. If there is no winner, the pre-existing value of winner (null).
-  // will be returned.
-  this.getWinner = function() {
-    winningSet = getWinningSet();
-    if (winningSet) {
-      winner = spots[winningSet[0]]
-    }
-    else if (this.getOpenSpots().length === 0) {
-      winner = 'Draw';
-    }
-    return winner;
-  }
-
-  // Return an array of 3 spots representing the winning play, or false if there's no winner or a draw.
-  function getWinningSet() {
-    var winningSets = [
-              [0, 1, 2], [3, 4, 5], [6, 7, 8],
-              [0, 3, 6], [1, 4, 7], [2, 5, 8],
-              [0, 4, 8], [2, 4, 6]
-              ],
-        winningSet = false;
-
-    winningSets.forEach( function(set){
-      if (spots[set[0]] == spots[set[1]] &&
-          spots[set[0]] == spots[set[2]] &&
-          spots[set[0]] !== null) {
-        winningSet = set;
-      }
-    })
-    return winningSet;
-  }
-
-  // Return an array of the spots that are still empty (null).
-  this.getOpenSpots = function() {
-    var openSpots = [];
-    for (var i = spots.length - 1; i >= 0; i--) {
-      if (spots[i] === null) {
-        openSpots.push(i);
-      }
-    }
-    return openSpots;
-  }
-
-  this.makeMove = function (spot) {
-    this.setOwner(spot, turn);
-    this.drawBoard();
-    this.goToNextTurn()
-  }
-
-  this.makeRandomMove = function() {
-    var random_move = Math.floor(Math.random() * (size * size - 1));
-    this.makeMove(random_move)
   }
 
   // Receive a move from the UI.
   this.selectMove = function(spot) {
     // Prevent a sneaky move from being made on computer's turn.
-    if (spots[spot] === null && winner === null && currentPlayerIsComputer() === false) {
+    if (spots[spot] === 0 && winner === 0 && currentPlayerIsComputer() === false) {
       this.makeMove(spot);
     }
   }
 
-  this.goToNextTurn = function() {
-    // If there’s no winner yet, change turn and act accordingly.
-    if (this.getWinner() === null) {
-      this.toggleTurn();
-      // if it's now the computer's turn, find the best move and make it.
-      if (currentPlayerIsComputer()) {
-        var that = this;
-        // Give it a delay so we can observe things happening
-        setTimeout(function () {
-          that.makeMove(that.getBestMove(), turn);
-        }, 200)
-      }
-      else {
-        announce(getPrompt())
-      }
+  this.makeMove = function(spot) {
+    this.setOwner(spot, turn);
+    drawBoard(spots, characters, this.getOpenSpots(), size);
+    this.checkGameStatus();
+    if (this.gameIsOver()) {
+      winner === 0 ? announceDraw() : announceWin(winningSet, winner)
     }
-
-    // But if there is a winner, process the endgame.
     else {
-      winningSet = getWinningSet()
-      announceWinner(winningSet);
+      this.toggleTurn()
+      this.goToNextTurn();
     }
   }
 
+  // Check to see if there is a winner yet. If there is (or if there’s a draw)
+  // update the values of winner and winningSet.
+  this.checkGameStatus = function() {
+    winningSets.forEach( function(set){
+      if (spots[set[0]] == spots[set[1]] &&
+          spots[set[0]] == spots[set[2]] &&
+          spots[set[0]] !== 0) {
+        winner = spots[set[0]];
+        winningSet = set;
+      }
+    })
+  }
+
   this.getBestMove = function() {
-    // For the sake of speed and user experience, let's return
-    // a pre-determined move for the second move of the game.
+    // For the sake of speed and user experience, let's return random or
+    // pre-determined moves for the first and second move of the game, respectively
+    if (this.getOpenSpots().length === (size * size)) {
+      return getRandomMove();
+    }
     if (this.getOpenSpots().length === (size * size - 1)) {
       return getBestSecondMove();
     }
 
     // If it's 0’s turn, we seek the move with the lowest value.
     // If it's 1’s turn, we seek the move with the highest.
-    var bestMove  = null,
+    var bestMove,
         openSpots = this.getOpenSpots(),
-        bestValue = turn === 0 ? 99999 : -99999;
+        bestValue = turn === 1 ? 99999 : -99999;
 
     for (var i = openSpots.length - 1; i >= 0; i--) {
       var move = openSpots[i],
           moveValue = this.valueOfPlay(move)
 
-      if (turn == 0 && moveValue < bestValue ||
-          turn == 1 && moveValue > bestValue) {
+      if (turn == 1 && moveValue < bestValue ||
+          turn == 2 && moveValue > bestValue) {
         bestValue = moveValue;
         bestMove = move;
       }
     }
     return bestMove;
-  }
-
-  function getBestSecondMove() {
-    var bestSecondMoves = [4, 0, 4, 0, 0, 8, 4, 6, 4],
-        firstMove;
-
-    firstMove = spots.indexOf(0) >= 0 ? spots.indexOf(0) : spots.indexOf(1);
-
-    return bestSecondMoves[firstMove];
   }
 
   // Returns a lower (negative) value for moves that favor Player 1,
@@ -235,13 +196,16 @@ function Board () {
                                                          // winning moves that occur earlier.
 
     tempBoard.setOwner(move, turn)
-    if (tempBoard.getWinner() !== null) {
-      if (tempBoard.getWinner() === 0) {
+    tempBoard.checkGameStatus();
+
+    if (tempBoard.gameIsOver()) {
+      if (tempBoard.getWinner() === 1) {
         score -= weight;
       }
-      else if (tempBoard.getWinner() === 1){
+      else if (tempBoard.getWinner() === 2){
         score += weight;
       }
+      tempBoard = null;
       return score;
     }
     else {
@@ -262,60 +226,99 @@ function Board () {
     return newBoard;
   }
 
-  this.resetBoard = function() {
-    spots = [
-      null, null, null,
-      null, null, null,
-      null, null, null
-    ];
-    winner = null;
-    turn = goesfirst;
-
-    this.drawBoard();
-    this.startPlay();
+  function getRandomMove() {
+    return Math.floor(Math.random() * (size * size - 1));
   }
 
-  this.startPlay = function() {
-    if (currentPlayerIsComputer()) {
-      this.makeRandomMove()
-    }
-    else {
-      announce(getPrompt());
-    }
-  }
+  function getBestSecondMove() {
+    var bestSecondMoves = [4, 0, 4, 0, 0, 8, 4, 6, 4],
+        firstMove;
 
-  function announceWinner (winningSet) {
-    document.getElementById('ttt-table').classList.add('game-over');
-    if (winner == "Draw") {
-      announce(winner + '! <em>Sometimes the only winning move is not to play.</em>')
-    }
-    else {
-      winningSet.forEach( function(spot) {
-        document.getElementById('pos-' + spot).classList.add('winner')
-        document.getElementById('pos-' + spot).classList.add('winner-' + winner)
-      })
-      announce(characters[winner] + ' has won!')
-    }
-  }
+    firstMove = spots.indexOf(1) >= 0 ? spots.indexOf(1) : spots.indexOf(2);
 
-  function announce(message) {
-    document.getElementById('announcement').innerHTML = message;
-  }
-
-  function getPrompt() {
-    return 'Player ' + characters[turn] + ', it is your turn.';
-  }
-
-  function clearAnnounce() {
-    document.getElementById('announcement').innerHTML = '';
-  }
-
-  this.showOptions = function() {
-    announce('Please choose from the following options for your game:');
-    document.getElementById('ttt-options').style.display = 'block';
-    document.getElementById('ttt-arena').style.display = 'none';
+    return bestSecondMoves[firstMove];
   }
 
 } // End Board()
+
+/*----------  Functions pertaining to the UI  ----------*/
+
+function announceDraw() {
+  document.getElementById('ttt-table').classList.add('game-over');
+  announcement.display('Draw! <em>Sometimes the only winning move is not to play.</em>');
+}
+
+function announceWin(winningSet, winner) {
+  document.getElementById('ttt-table').classList.add('game-over');
+  winningSet.forEach( function(spot) {
+    document.getElementById('pos-' + spot).classList.add('winner')
+    document.getElementById('pos-' + spot).classList.add('winner-' + winner)
+  })
+  announcement.display('Player ' + winner + ' has won!')
+}
+
+function drawBoard(spots, characters, openSpots, size) {
+  announcement.clear()
+  for (var i = spots.length - 1; i >= 0; i--) {
+    document.getElementById('pos-' + i).innerHTML = characters[spots[i]] || '&nbsp;';
+    if (spots[i] !== 0) {
+      document.getElementById('pos-' + i).className += " taken";
+    }
+  }
+  // If this is an empty board, reset a few things.
+  if (openSpots.length === (size * size)) {
+    document.getElementById('reset-button').classList.remove('active');
+    document.getElementById('ttt-table').classList.remove('game-over');
+    removeClass('taken');
+    removeClass('winner');
+    removeClass('winner-1');
+    removeClass('winner-2');
+  }
+  else {
+    document.getElementById('reset-button').classList.add('active');
+  }
+}
+
+function getPrompt(turn) {
+  return 'Player ' + turn + ', it is your turn.';
+}
+
+function showOptions() {
+  announcement.display('Please choose from the following options for your game:');
+  document.getElementById('ttt-options').style.display = 'block';
+  document.getElementById('ttt-arena').style.display = 'none';
+}
+
+function removeClass(classname) {
+  var els = document.getElementsByClassName(classname);
+  while (els.length > 0) {
+    els[0].classList.remove(classname);
+  }
+}
+
+function validateCharacters(characterA, characterB) {
+  if (characterA.length === 0) {
+    return 'Player 1 needs a character.';
+  }
+  if (characterB.length === 0) {
+    return 'Player 2 needs a character.';
+  }
+  if (characterA === characterB) {
+    return 'It looks like both players have the same character. Please change one.'
+  }
+  return false;
+}
+
+var announcement = {
+  el: document.getElementById('announcement'),
+
+  display: function(message) {
+    this.el.innerHTML = message;
+  },
+
+  clear: function() {
+    this.el.innerHTML = '';
+  }
+}
 
 var board = new Board();
--- tttSpec.js
+++ tttSpec.js
@@ -1,40 +1,340 @@
 describe("Tic Tac Toe", function() {
 
+  it("knows when the game is over", function() {
+    var board = new Board();
+    board.setSpots([2,2,2,
+                    1,0,0,
+                    0,0,1])
+    board.checkGameStatus();
+    expect(board.gameIsOver()).toBe(true)
+
+    board = new Board();
+    board.setSpots([2,1,2,
+                    1,1,2,
+                    1,2,1])
+    board.checkGameStatus();
+    expect(board.gameIsOver()).toBe(true)
+
+    board = new Board();
+    board.setSpots([0,0,0,
+                    0,0,0,
+                    0,0,0])
+    board.checkGameStatus();
+    expect(board.gameIsOver()).toBe(false)
+
+    board = new Board();
+    board.setSpots([2,0,2,
+                    1,2,0,
+                    0,0,1])
+    board.checkGameStatus();
+    expect(board.gameIsOver()).toBe(false)
+  })
 
   it("recognizes a winner", function() {
+    var board = new Board();
+
+    board.setSpots([2,2,2,
+                    1,0,0,
+                    0,0,1])
+    board.checkGameStatus();
+    expect(board.hasWinner()).toBe(true);
+    expect(board.getWinner()).toBe(2);
+
     board = new Board();
+    board.setSpots([2,1,0,
+                    2,0,1,
+                    2,0,1])
+    board.checkGameStatus();
+    expect(board.hasWinner()).toBe(true);
+    expect(board.getWinner()).toBe(2);
 
-    board.setSpots([0,0,0,
-      1,null,null,
-      null,null,1])
-    expect(board.getWinner()).toBe(0);
-    expect(board.getOpenSpots().length).toBe(4);
-
-    board.setSpots([0,1,null,
-      0,null,1,
-      0,null,1])
-    expect(board.getWinner()).toBe(0);
-    expect(board.getOpenSpots().length).toBe(3);
-
-    board.setSpots([1,1,null,
-      0,1,1,
-      0,null,1])
+    board = new Board();
+    board.setSpots([1,2,0,
+                    2,1,2,
+                    2,0,1])
+    board.checkGameStatus();
+    expect(board.hasWinner()).toBe(true);
     expect(board.getWinner()).toBe(1);
-    expect(board.getOpenSpots().length).toBe(2);
+
+    board = new Board();
+    board.setSpots([1,2,0,
+                    2,0,2,
+                    2,0,1])
+    board.checkGameStatus();
+    expect(board.hasWinner()).toBe(false);
+    expect(board.getWinner()).toBe(0);
+
+    board = new Board();
+    board.setSpots([0,0,0,
+                    0,0,0,
+                    0,0,0])
+    board.checkGameStatus();
+    expect(board.hasWinner()).toBe(false);
+    expect(board.getWinner()).toBe(0);
+
+    board = new Board();
+    board.setSpots([1,2,1,
+                    1,1,2,
+                    2,1,2])
+    board.checkGameStatus();
+    expect(board.hasWinner()).toBe(false);
+    expect(board.getWinner()).toBe(0);
+
   });
+
+  it("count empty spots", function() {
+    var board = new Board();
+    board.setSpots([1,0,0,
+                0,0,0,
+                0,0,0])
+    expect(board.getOpenSpots().length).toBe(8)
+
+    board.setSpots([1,0,0,
+                0,0,2,
+                0,0,0])
+    expect(board.getOpenSpots().length).toBe(7)
+
+    board.setSpots([1,1,0,
+                0,0,2,
+                0,0,0])
+    expect(board.getOpenSpots().length).toBe(6)
+
+    board.setSpots([1,1,2,
+                0,0,2,
+                0,0,0])
+    expect(board.getOpenSpots().length).toBe(5)
+
+    board.setSpots([1,1,2,
+                1,0,2,
+                0,0,0])
+    expect(board.getOpenSpots().length).toBe(4)
+
+    board.setSpots([1,1,2,
+                1,2,2,
+                0,0,0])
+    expect(board.getOpenSpots().length).toBe(3)
+
+    board.setSpots([1,1,2,
+                1,2,2,
+                2,0,0])
+    expect(board.getOpenSpots().length).toBe(2)
+
+    board.setSpots([1,1,2,
+                1,2,2,
+                2,1,0])
+    expect(board.getOpenSpots().length).toBe(1)
+
+    board.setSpots([1,1,2,
+                1,2,2,
+                2,1,2])
+    expect(board.getOpenSpots().length).toBe(0)
+
+  })
 
   it("gets the right best move", function() {
+    var move,
+        board = new Board();
+
+    // Best Second move
+    board.setTurn(2);
+    board.setSpots([1,0,0,
+                    0,0,0,
+                    0,0,0]);
+    move = board.getBestMove();
+    expect(move).toBe(4);
+
     board = new Board();
+    board.setTurn(2);
+    board.setSpots([0,1,0,
+                    0,0,0,
+                    0,0,0]);
+    move = board.getBestMove();
+    expect(move === 0 || move === 1).toBe(true);
 
-    board.setSpots([0,null,0,
-      1,null,null,
-      null,null,1])
-    board.setTurn(0)
-    expect(board.getBestMove()).toBe(1);
-    expect(board.getOpenSpots().length).toBe(5);
+    board = new Board();
+    board.setTurn(2);
+    board.setSpots([0,0,1,
+                    0,0,0,
+                    0,0,0]);
+    move = board.getBestMove();
+    expect(move).toBe(4);
 
+    board = new Board();
+    board.setTurn(2);
+    board.setSpots([0,0,0,
+                    1,0,0,
+                    0,0,0]);
+    move = board.getBestMove();
+    expect(move === 0 || move === 6).toBe(true);
+
+    board = new Board();
+    board.setTurn(2);
+    board.setSpots([0,0,0,
+                    0,1,0,
+                    0,0,0])
+    move = board.getBestMove();
+    expect(move === 0 || move === 2 || move === 6 || move === 8).toBe(true);
+
+    board = new Board();
+    board.setTurn(2);
+    board.setSpots([0,0,0,
+                    0,0,1,
+                    0,0,0]);
+    move = board.getBestMove();
+    expect(move === 2 || move === 8).toBeTruthy();
+
+    board = new Board();
+    board.setTurn(2);
+    board.setSpots([0,0,0,
+                    0,0,0,
+                    1,0,0]);
+    move = board.getBestMove();
+    expect(move).toBe(4);
+
+    board = new Board();
+    board.setTurn(2);
+    board.setSpots([0,0,0,
+                    0,0,0,
+                    0,1,0]);
+    move = board.getBestMove();
+    expect(move === 6 || move === 8).toBeTruthy();
+
+    board = new Board();
+    board.setTurn(2);
+    board.setSpots([0,0,0,
+                    0,0,0,
+                    0,0,1]);
+    move = board.getBestMove();
+    expect(move).toBe(4);
+
+    // Best second move, if player 2 goes first
+    board = new Board();
+    board.setSpots([2,0,0,
+                    0,0,0,
+                    0,0,0]);
+    move = board.getBestMove();
+    expect(move).toBe(4);
+
+    board = new Board();
+    board.setSpots([0,2,0,
+                    0,0,0,
+                    0,0,0]);
+    move = board.getBestMove();
+    expect(move === 0 || move === 1).toBeTruthy();
+
+    board = new Board();
+    board.setSpots([0,0,2,
+                    0,0,0,
+                    0,0,0]);
+    move = board.getBestMove();
+    expect(move).toBe(4);
+
+    board = new Board();
+    board.setSpots([0,0,0,
+                    2,0,0,
+                    0,0,0]);
+    move = board.getBestMove();
+    expect(move === 0 || move === 6).toBeTruthy();
+
+    board = new Board();
+    board.setSpots([0,0,0,
+                    0,2,0,
+                    0,0,0])
+    move = board.getBestMove();
+    expect(move === 0 || move === 2 || move === 6 || move === 8).toBeTruthy();
+
+    board = new Board();
+    board.setSpots([0,0,0,
+                    0,0,2,
+                    0,0,0]);
+    move = board.getBestMove();
+    expect(move === 2 || move === 8).toBeTruthy();
+
+    board = new Board();
+    board.setSpots([0,0,0,
+                    0,0,0,
+                    2,0,0]);
+    move = board.getBestMove();
+    expect(move).toBe(4);
+
+    board = new Board();
+    board.setSpots([0,0,0,
+                    0,0,0,
+                    0,2,0]);
+    move = board.getBestMove();
+    expect(move === 6 || move === 8).toBeTruthy();
+
+    board = new Board();
+    board.setSpots([0,0,0,
+                    0,0,0,
+                    0,0,2]);
+    move = board.getBestMove();
+    expect(move).toBe(4);
+
+
+    // Defends against defeat
+    board = new Board();
+    board.setSpots([0,2,2,
+                    1,0,0,
+                    0,0,1]);
+    board.setTurn(1);
+    move = board.getBestMove();
+    expect(move).toBe(0);
+
+    board = new Board();
+    board.setSpots([2,0,2,
+                    1,0,0,
+                    1,0,1]);
+    board.setTurn(2);
+    move = board.getBestMove();
+    expect(move).toBe(1);
+
+    board = new Board();
+    board.setSpots([0,0,2,
+                    1,0,2,
+                    2,1,1]);
+    board.setTurn(1);
+    move = board.getBestMove();
+    expect(move).toBe(4);
+
+    // Takes victory when it can
+    board = new Board();
+    board.setSpots([2,0,2,
+                    0,0,0,
+                    1,0,1]);
+    board.setTurn(1);
+    move = board.getBestMove();
+    expect(move).toBe(7);
+
+    board = new Board();
+    board.setSpots([2,1,2,
+                    1,1,0,
+                    2,2,1]);
+    board.setTurn(1);
+    move = board.getBestMove();
+    expect(move).toBe(5);
+
+    board = new Board();
+    board.setSpots([1,1,2,
+                    1,0,0,
+                    2,0,2]);
+    board.setTurn(2);
+    move = board.getBestMove();
+    expect(move === 7 || move === 5 || move === 4).toBeTruthy();
+
+
+    // Sets up a checkmate
+    board = new Board();
+    board.setSpots([0,2,1,
+                    0,0,2,
+                    0,0,1]);
+    board.setTurn(1);
+    move = board.getBestMove();
+    expect(move === 6 || move === 4).toBeTruthy();
+
+    board.setTurn(2)
+    move = board.getBestMove();
+    expect(move).toBe(4);
 
   });
-
-
-});
+}); // End Describe("Tic Tac Toe")
